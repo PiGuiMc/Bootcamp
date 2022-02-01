3e5b06113cf19566dd6af6bfed3e3e095e3d9863
PiGuiMc <guimacedocosta@gmail.com>
2022-02-01 09:19:10 -0300

    new commit

--- JavaScript/assets/js/script.js
+++ JavaScript/assets/js/script.js
@@ -1,12 +1,33 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
+var btnAdd = document.getElementById("Add");
+var btnDesc = document.getElementById("Desc");
 
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber == 10){
+        btnAdd.disabled = true;
+    }
+    if(currentNumber > -10){
+        btnDesc.disabled = false;
+    }
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = 'blue';
+    }
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+
+    if(currentNumber < 10){
+        btnAdd.disabled = false;
+    }
+    if(currentNumber == -10){
+        btnDesc.disabled = true;
+    }
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
 }
