77d37b51925bcf24b80cce767c8b45570a7d9dca
PiGuiMc <guimacedocosta@gmail.com>
2022-02-10 08:56:30 -0300

    :fire: new commit

---
+++ Bootcamp_JavaScript/js.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('stdin','utf8');
+var lines = input.split('\n')
+
+for(let i=0; i<7; i++){
+    var n = parseInt(lines.shift());
+
+    const teste = (n) => {
+        if(n % 2 == 0){
+            return "par";
+        }else{
+            return "impar";
+        }
+    }
+
+    console.log(teste(n))
+}
+
+
